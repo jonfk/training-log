df02969c27f0e6754f3dd693bd89c9b02118a710
Jonathan Fok kan <jfokkan@inocybe.com>
2015-03-11 21:28:53 -0400

    ADDED statistics
    REFACTORED validator structs to common.go
    ADDED makefile

--- progs/validator.go
+++ progs/validator.go
@@ -10,26 +10,6 @@ import (
 	"strings"
 )
 
-type Exercise struct {
-	Name     string `name`
-	Weight   string `weight,omitempty`
-	Sets     string `sets`
-	Reps     string `reps`
-	Exertion string `exertion`
-}
-
-type TrainingLog struct {
-	Date       string     `date`
-	Time       string     `time,omitempty`
-	Length     string     `length,omitempty`
-	Bodyweight string     `bodyweight,omitempty`
-	Event      string     `event,omitempty`
-	Wilks      string     `wilks,omitempty`
-	Total      string     `total,omitempty`
-	Workout    []Exercise `workout`
-	Notes      []string   `notes,omitempty`
-}
-
 var Verbose bool = false
 var Flexible bool = false
 var Output bool = false
